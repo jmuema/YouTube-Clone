220d79f21025c9298ca7d546b3b3cf7ce50359ef
muema254 <jojotheone7@gmail.com>
2020-10-11 20:42:34 +0300

    added containers for styling

--- src/Header.js
+++ src/Header.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import './Header.css';
-import VideoCallSharpIcon from '@material-ui/icons/VideoCallSharp';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import AppsSharpIcon from '@material-ui/icons/AppsSharp';
-import NotificationsSharpIcon from '@material-ui/icons/NotificationsSharp';
-import Avatar from '@material-ui/core/Avatar';
-
-
+import React from "react";
+import "./Header.css";
+import VideoCallSharpIcon from "@material-ui/icons/VideoCallSharp";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AppsSharpIcon from "@material-ui/icons/AppsSharp";
+import NotificationsSharpIcon from "@material-ui/icons/NotificationsSharp";
+import Avatar from "@material-ui/core/Avatar";
 
 function Header() {
-    return (
-        <div className="header" >
-            {/* <h1>I am a Header</h1> */}
-            <MenuIcon/>
-            <img  
-            className="header__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png" />
-            <input type="text"
-            alt=""
-            
-            />
-            <SearchIcon  />
-            <VideoCallSharpIcon/>
-            <AppsSharpIcon/>
-            <NotificationsSharpIcon/>
-            <Avatar 
-            alt="Muema"
-            src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png "
-            
-            />
-        </div>
-    )
+  return (
+    <div className="header">
+      {/* <h1>I am a Header</h1> */}
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
+        />
+      </div>
+      <div className="header__input">
+        <input type="text" alt="" />
+        <SearchIcon />
+      </div>
+
+      <div className="header__icons">
+        <VideoCallSharpIcon />
+        <AppsSharpIcon />
+        <NotificationsSharpIcon />
+        <Avatar
+          alt="Muema"
+          src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png "
+        />
+      </div>
+    </div>
+  );
 }
- 
-export default Header
+
+export default Header;

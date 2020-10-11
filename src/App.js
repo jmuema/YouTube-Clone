6184db2fde0df1209c0ccc4cb58d5271e3dbc3fd
muema254 <jojotheone7@gmail.com>
2020-10-11 20:35:34 +0300

    styling the header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <h1> YouTube Clone</h1>
+      {/* <h1> YouTube Clone</h1> */}
 
 
       {/*  Header */}

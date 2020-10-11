111a93885094a8949c4ea50ce174590aabf69700
muema254 <jojotheone7@gmail.com>
2020-10-11 20:18:05 +0300

    created the header component

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,8 @@ function Header() {
             <img  
             className="header__logo"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png" />
+            <input type="text" />
+            
         </div>
     )
 }

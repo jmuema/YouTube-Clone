30fed6fa09e92d6e93cf4b0e19618aab3ff4df19
muema254 <jojotheone7@gmail.com>
2020-10-19 07:49:46 +0300

    finished up on the styling on recommended videos

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -8,11 +8,11 @@ function VideoCard(props) {
 
 
     return (
-        <div className="videocard" >
-            <img src={image} alt=""/>
-            <div className="video__info" > 
+        <div className="videoCard" >
+            <img className="videoCard__thumbnail" src={image} alt=""/>
+            <div className="videoCard__info" > 
             <Avatar className="videoCard__avatar" alt={channel} src={channelImage} />
-            <div className="video__text" > 
+            <div className="videoCard__text" > 
             <h4>{title} </h4>
             <p>{channel} </p>
             <p> {views} .
